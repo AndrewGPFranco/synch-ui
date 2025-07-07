7bb3a4d116311571f796adae57aecb44e75e93b9
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-07-07 07:05:49 -0300

    feat: componente com a barra de pesquisa e alguns filtros possíveis

---
+++ src/components/Finance/SearchFilter.vue
@@ -0,0 +1,52 @@
+<template>
+    <section class="container">
+        <input type="search" placeholder="Pesquisar" class="inputSearch">
+
+        <n-date-picker type="date" placeholder="Data" class="selectDate" />
+
+        <n-select class="selectStatus" v-model:value="selectedStatus" filterable placeholder="Status"
+            :options="optionsStatus" />
+
+        <n-select class="selectFilter" v-model:value="selectedFilter" filterable placeholder="Filtro"
+            :options="optionsFilter" />
+    </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { NDatePicker, NSelect } from 'naive-ui'
+
+const selectedFilter = ref<string | null>(null)
+const selectedStatus = ref<string | null>(null)
+
+const optionsStatus = ref([
+    { label: "Todos", value: "Todos" },
+    { label: "Pausados", value: "Pausados" },
+    { label: "Importantes", value: "Importantes" }
+])
+
+const optionsFilter = ref([
+    { label: "Filtro 1", value: "Filtro 1" },
+    { label: "Filtro 2", value: "Filtro 2" },
+    { label: "Filtro 3", value: "Filtro 3" }
+])
+</script>
+
+<style scoped lang="scss">
+.container {
+    display: flex;
+    padding: 1.3em;
+    justify-content: space-between;
+
+    .inputSearch {
+        padding: 0.5em;
+        width: 60vw;
+    }
+
+    .selectStatus,
+    .selectFilter,
+    .selectDate {
+        width: 12vw;
+    }
+}
+</style>
--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,10 +1,11 @@
 <template>
     <Navbar />
     <OptionsTable />
+    <SearchFilter />
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/components/Finance/Navbar.vue';
 import OptionsTable from '@/components/Finance/OptionsTable.vue';
-
+import SearchFilter from '@/components/Finance/SearchFilter.vue';
 </script>
