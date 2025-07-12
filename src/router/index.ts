970ae43b100a792fc457d75343fe619f5f2ba326
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-07-12 01:26:26 -0300

    feat: adicionando título dinâmico

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,29 +7,42 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component:  () => import('@/views/HomeView.vue')
+      component: () => import('@/views/HomeView.vue'),
+      meta: {
+        title: 'Synch - Home',
+      },
     },
     {
-      path: '/login',
+      path: '/auth',
       name: 'login',
-      component: () => import('@/views/AuthView.vue')
+      component: () => import('@/views/AuthView.vue'),
+      meta: {
+        title: 'Synch - Autenticação',
+      },
     },
     {
       path: '/tables',
       name: 'tables',
       component: () => import('@/views/TableListingView.vue'),
+      meta: {
+        title: 'Synch - Tabelas',
+      },
     },
     {
       path: '/table/:id',
       name: 'table-detail',
       component: () => import('@/views/TableView.vue'),
-    }
+      meta: {
+        title: 'Synch - Visualização Tabela',
+      },
+    },
   ],
 })
 
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore()
   const isAuthenticated: boolean = authStore.isUserAutenticado()
+  document.title = (to.meta?.title as string) ?? 'Synch'
 
   const publicRoutes = ['login', 'registerUser']
 
