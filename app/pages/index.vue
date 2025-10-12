07a2504db3334a056b879ff70446b11458ffd264
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-10-12 20:54:17 -0300

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Bem vindo!</h1>
+    <USeparator />
+    <UAvatar src="https://github.com/AndrewGPFranco.png" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
