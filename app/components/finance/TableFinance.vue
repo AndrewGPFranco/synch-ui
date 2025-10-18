c5a1cd64a008ff5d6b1c341456ca9eac91c2c21d
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-10-18 03:48:08 -0300

    feat: formatando alguns dados de exibição da tabela

--- app/components/finance/TableFinance.vue
+++ app/components/finance/TableFinance.vue
@@ -4,7 +4,7 @@
 
 <script setup lang="ts">
 import type {IExpense} from '~/types/IExpense';
-import type {TableColumn} from '#ui/components/Table.vue';
+import type {TableColumn, TableRow} from '#ui/components/Table.vue';
 
 const props = defineProps({
   expenses: {
@@ -13,24 +13,85 @@ const props = defineProps({
   }
 });
 
+const getPaymentCategory = (row: TableRow<IExpense>): string => {
+  switch (row.getValue("paymentCategory")) {
+    case "FIXED":
+      return "Despesa FIXA";
+    case "VARIABLE":
+      return "Despesa variável";
+    case "UNFORESEEN":
+      return "Despesa imprevísivel";
+    default:
+      return "Categoria desconhecida";
+  }
+}
+
+const getMonth = (row: TableRow<IExpense>): string => {
+  switch (row.getValue("month")) {
+    case "JANUARY":
+      return "Janeiro";
+    case "FEBRUARY":
+      return "Fevereiro";
+    case "MARCH":
+      return "Março";
+    case "APRIL":
+      return "Abril";
+    case "MAY":
+      return "Maio";
+    case "JUNE":
+      return "Junho";
+    case "JULY":
+      return "Julho";
+    case "AUGUST":
+      return "Agosto";
+    case "SEPTEMBER":
+      return "Setembro";
+    case "OCTOBER":
+      return "Outubro";
+    case "NOVEMBER":
+      return "Novembro";
+    case "DEZEMBER":
+      return "Dezembro";
+    default:
+      return "Mês desconhecido!"
+  }
+}
+
 const columns: TableColumn<IExpense>[] = [
   {accessorKey: 'name', header: 'Nome'},
-  {accessorKey: 'month', header: 'Mês'},
-  {accessorKey: 'amount', header: 'Valor'},
-  {accessorKey: 'paymentCategory', header: 'Categoria'},
+  {
+    accessorKey: 'month',
+    header: 'Mês',
+    cell: ({row}) => {
+      return getMonth(row);
+    }
+  },
+  {
+    accessorKey: 'amount',
+    header: 'Valor',
+    cell: ({row}) => {
+      const value = row.getValue("amount") as number;
+      return Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(value)
+    }
+  },
+  {
+    accessorKey: 'paymentCategory',
+    header: 'Categoria',
+    cell: ({row}) => {
+      return getPaymentCategory(row);
+    }
+  },
   {
     accessorKey: 'paymentDate',
     header: 'Data do Pagamento',
-    cell: ({row}) => new Date(row.getValue('paymentDate')).toLocaleString('pt-BR', {
-      day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit'
-    })
+    cell: ({row}) => new Date(row.getValue('paymentDate'))
+        .toLocaleString('pt-BR', {day: 'numeric', month: 'short', year: 'numeric'})
   },
   {
     accessorKey: 'dueDate',
     header: 'Data de Vencimento',
-    cell: ({row}) => new Date(row.getValue('dueDate')).toLocaleString('pt-BR', {
-      day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit'
-    })
+    cell: ({row}) => new Date(row.getValue('dueDate'))
+        .toLocaleString('pt-BR', {day: 'numeric', month: 'short', year: 'numeric'})
   },
 ];
 
@@ -46,7 +107,7 @@ onUnmounted(() => {
   window.removeEventListener('popstate', handleBackButton)
 })
 
-const handleBackButton = (event: PopStateEvent) => {
+const handleBackButton = () => {
   router.push('/')
 }
 </script>
