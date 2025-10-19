773c8389fd00c52f8ba0ca759c2bee8e6c2a4560
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-10-19 00:34:44 -0300

    chore: nuxt icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: true},
-    modules: ['@nuxt/ui', '@pinia/nuxt'],
+    modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/icon'],
     css: ['~/assets/css/main.css'],
     app: {
         head: {
